6e98a779707d109c6ae5f94cded6d0f7ff4e6ba1
Noor Chasib <noorchasib@gmail.com>
2022-11-23 16:00:33 -0800

    update

---
+++ constants.js
@@ -0,0 +1,20 @@
+// connection
+const IP = '165.227.47.243';
+const PORT = 50541;
+
+//key mappings
+const keyMapping = {
+  w : "Move: up",
+  a : "Move: left",
+  s : "Move: down",
+  d : "Move: right",
+  z : "Say: yoyoyo",
+  x : "Say: i like big snakes",
+  c : "Say: and i cannot lie"
+};
+
+module.exports = {
+  IP,
+  PORT,
+  keyMapping
+};
--- input.js
+++ input.js
@@ -1,31 +1,12 @@
 let connection;
+const { keyMapping } = require("./constants");
 
 const handleUserInput = function(key) {
   // stdin.on('data', (key) => {
   if (key === '\u0003') {
     process.exit();
   }
-  if (key === 'w') {
-    connection.write("Move: up");
-  }
-  if (key === 'a') {
-    connection.write("Move: left");
-  }
-  if (key === 's') {
-    connection.write("Move: down");
-  }
-  if (key === 'd') {
-    connection.write("Move: right");
-  }
-  if (key === 'z') {
-    connection.write("Say: yoyoyo");
-  }
-  if (key === 'x') {
-    connection.write("Say: i like big snakes");
-  }
-  if (key === 'c') {
-    connection.write("Say: and i cannot lie");
-  }
+  connection.write(keyMapping[key]);
   // });
 };
 
